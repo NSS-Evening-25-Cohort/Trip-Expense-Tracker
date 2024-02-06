4aa179bb053f16fea3a2c994e5dfb09c91b01dcf
max rubel <maxrubel1@gmail.com>
2024-02-05 22:09:24 -0600

    expense is submitting, but categories situation is still being worked out

--- pages/trip/new.js
+++ pages/trip/new.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
 import { useRouter } from 'next/router';
 // eslint-disable-next-line import/no-unresolved
-import TripForm from '../../components/tripForm';
+import TripForm from '../../components/TripForm';
 
 export default function NewTrip() {
   const router = useRouter();
